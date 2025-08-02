0a54a230dfc9859e7c25274f6f301aeef723b7e3
Slats <michael.john.slattery@gmail.com>
2025-08-02 10:06:00 -0400

    Added Health Check Enpoints

--- api/v1/endpoints.py
+++ api/v1/endpoints.py
@@ -4,6 +4,12 @@ from auth import get_current_user # Import the shared dependency
 
 router = APIRouter()
 
+# Healthcheck endpoint
+@router.get("/hc", description="Healthcheck endpoint")
+@router.get("/health", description="Healthcheck endpoint")
+async def healthcheck():
+    return {"status": "ok"} 
+
 @router.get("/items/{item_id}", description="Get an item by its ID")
 async def read_item_v1(
     item_id: int,
